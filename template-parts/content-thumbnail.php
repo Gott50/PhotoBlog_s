ed0308a14e8e96f74d7d7becfc083a6388920632
Gott50 <xxGott50xx@gmail.com>
2017-09-20 00:50:17 +0200

    make header.php more compatible with changes to WordPress
    by adding a Wrapper

--- template-parts/content-thumbnail.php
+++ template-parts/content-thumbnail.php
@@ -13,8 +13,10 @@
 	<?php if ( has_post_thumbnail() ):
 		$urlImg = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
 	endif; ?>
-
-    <header class="entry-header thumbnail" style="background-image: url(<?php echo $urlImg; ?>);">
+    <div class="thumbnail">
+		<?php echo get_the_post_thumbnail(); ?>
+    </div>
+    <header class="entry-header">
 		<?php
 		if ( is_singular() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
