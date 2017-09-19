9c90a08422d54c0e5d190fbfbcf165cd10a2af45
Gott50 <xxGott50xx@gmail.com>
2017-09-19 18:15:50 +0200

    refactor

--- header.php
+++ header.php
@@ -13,50 +13,53 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-    <title><?php bloginfo('name'); ?><?php wp_title('-'); ?></title>
-    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php bloginfo( 'name' ); ?><?php wp_title( '-' ); ?></title>
+    <meta name="description" content="<?php bloginfo( 'description' ); ?>">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>"/>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'photoblog_s' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'photoblog_s' ); ?></a>
 
-	<header id="masthead" class="site-header">
+    <header id="masthead" class="site-header">
         <div class="custom-header-media">
 			<?php the_custom_header_markup(); ?>
         </div>
-		<div class="site-branding">
+        <div class="site-branding">
 			<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                                          rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                                         rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
 			endif;
 
 			$description = get_bloginfo( 'description', 'display' );
 			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
+                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
 			endif; ?>
-		</div><!-- .site-branding -->
+        </div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'photoblog_s' ); ?></button>
+        <nav id="site-navigation" class="main-navigation">
+            <button class="menu-toggle" aria-controls="primary-menu"
+                    aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'photoblog_s' ); ?></button>
 			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'primary',
-					'menu_id'        => 'primary-menu',
-				) );
+			wp_nav_menu( array(
+				'theme_location' => 'primary',
+				'menu_id'        => 'primary-menu',
+			) );
 			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+        </nav><!-- #site-navigation -->
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
