39f94e3d0a09d3e895112ffe0ad3763ba9fb5a0a
Gott50 <xxGott50xx@gmail.com>
2017-09-24 18:27:42 +0200

    pull <a> up

--- header.php
+++ header.php
@@ -32,22 +32,23 @@
 		    <?php the_custom_header_markup(); ?>
         </div>
         <div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
-                                          rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
-                                         rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                <?php
+                the_custom_logo();
+                if ( is_front_page() && is_home() ) : ?>
+                    <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+                <?php else : ?>
+                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+                                             rel="home"><?php bloginfo( 'name' ); ?></p>
+                    <?php
+                endif;
 
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-				<?php
-			endif; ?>
+                $description = get_bloginfo( 'description', 'display' );
+                if ( $description || is_customize_preview() ) : ?>
+                    <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                    <?php
+                endif; ?>
+            </a>
         </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="main-navigation">
