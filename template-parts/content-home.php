1c6ebd44b1428bb495829e21f3f2fe92658017bf
Gott50 <xxGott50xx@gmail.com>
2017-09-24 14:53:10 +0200

    pull <a> up

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -10,14 +10,16 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif; ?>
-    </header><!-- .entry-header -->
+    <a href="' . esc_url( get_permalink() ) . '" rel="bookmark">
+        <header class="entry-header">
+            <?php
+            if ( is_singular() ) :
+                the_title( '<h1 class="entry-title">', '</h1>' );
+            else :
+                the_title( '<h2 class="entry-title">', '</h2>' );
+            endif; ?>
+        </header><!-- .entry-header -->
+    </a>
     <div class="thumbnail">
 	    <?php the_post_thumbnail(); ?>
     </div><!-- .thumbnail -->
