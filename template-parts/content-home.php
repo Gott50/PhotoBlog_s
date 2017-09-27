5bf8bd0bebda85453cea8d6d9f38d845e891ca6d
Gott50 <xxGott50xx@gmail.com>
2017-09-27 15:48:50 +0200

    move Title of PreviewPost into a BlackBox under Thumbnail

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -11,16 +11,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-        <header class="entry-header">
-            <?php
-            if ( is_singular() ) :
-                the_title( '<h1 class="entry-title">', '</h1>' );
-            else :
-                the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark"><h2 class="entry-title">', '</h2> </a>' );
-            endif; ?>
-        </header><!-- .entry-header -->
-
     <div class="thumbnail">
-	    <?php the_post_thumbnail(); ?>
+		<?php the_post_thumbnail(); ?>
     </div><!-- .thumbnail -->
+
+    <header class="entry-header">
+		<?php
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark"><h2 class="entry-title">', '</h2> </a>' );
+		endif; ?>
+    </header><!-- .entry-header -->
 </article><!-- #post-<?php the_ID(); ?> -->
