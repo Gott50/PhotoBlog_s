b29c439d8190044268566385b55355b9d1e438c8
Gott50 <xxGott50xx@gmail.com>
2017-12-13 18:45:16 +0100

    use PostNavigation with separated Links

--- single.php
+++ single.php
@@ -9,6 +9,11 @@
 
 get_header(); ?>
 
+<div class="post-navigation">
+    <?php previous_post_link(); ?>
+    <?php next_post_link(); ?>
+</div>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
@@ -16,8 +21,6 @@ get_header(); ?>
 		while ( have_posts() ) : the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
