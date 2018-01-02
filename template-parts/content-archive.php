2446b43756b7b1ea333e085fd113bb944cf541d7
Gott50 <xxGott50xx@gmail.com>
2018-01-02 13:42:08 +0100

    add Post like Styling to .archive

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -29,7 +29,7 @@
 
 	<div class="entry-content">
         <?php if (has_post_thumbnail()): ?>
-            <div class="thumbnail">
+            <div class="thumbnail alignleft">
                 <?php the_post_thumbnail('medium'); ?>
             </div>
         <?php endif; ?>
