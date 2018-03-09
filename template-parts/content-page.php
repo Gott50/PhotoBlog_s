2bfbd7fdc55d79f5559b30f48f135b737377ae97
Gott50 <xxGott50xx@gmail.com>
2018-03-09 10:07:22 +0100

    add MetaDate to Page

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,6 +12,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <div class="entry-meta">
+			<?php photoblog_s_posted_on(); ?>
+        </div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
