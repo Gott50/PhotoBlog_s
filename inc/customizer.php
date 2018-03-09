153acd92036c16944989236d5a37d7047074336b
Gott50 <xxGott50xx@gmail.com>
2018-03-09 11:03:18 +0100

    testing

--- inc/customizer.php
+++ inc/customizer.php
@@ -14,6 +14,9 @@ function photoblog_s_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->default = '#123456';
+	$wp_customize->get_setting( 'background_color' )->transport = 'postMessage';
+	$wp_customize->get_setting( 'background_color' )->default = '#123456';
 
 	if ( isset( $wp_customize->selective_refresh ) ) {
 		$wp_customize->selective_refresh->add_partial( 'blogname', array(
@@ -24,7 +27,38 @@ function photoblog_s_customize_register( $wp_customize ) {
 			'selector'        => '.site-description',
 			'render_callback' => 'photoblog_s_customize_partial_blogdescription',
 		) );
+
+
+		$wp_customize->selective_refresh->add_partial( 'link_textcolor', array(
+			'selector'        => '.main-navigation',
+		) );
 	}
+
+	/**
+	 * TEST
+	 */
+	//2. Register new settings to the WP database...
+	$wp_customize->add_setting( 'link_textcolor', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
+		array(
+			'default'    => '#2BA6CB', //Default setting/value to save
+			'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
+			'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
+			'transport'  => 'postMessage', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
+		)
+	);
+
+	//3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
+	$wp_customize->add_control( new WP_Customize_Color_Control( //Instantiate the color control class
+		$wp_customize, //Pass the $wp_customize object (required)
+		'mytheme_link_textcolor', //Set a unique ID for the control
+		array(
+			'label'      => 'Link Color', //Admin-visible name of the control
+			'settings'   => 'link_textcolor', //Which setting to load and manipulate (serialized is okay)
+			'priority'   => 10, //Determines the order this control appears in for the specified section
+			'section'    => 'colors', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
+		)
+	) );
+
 }
 add_action( 'customize_register', 'photoblog_s_customize_register' );
 
