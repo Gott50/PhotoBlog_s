69ea675d2080d03825dc368610487802242e3b34
Gott50 <xxGott50xx@gmail.com>
2018-03-24 08:58:30 +0100

    rename navigation.js to comments.js

--- functions.php
+++ functions.php
@@ -131,7 +131,7 @@ add_action( 'widgets_init', 'photoblog_s_widgets_init' );
 function photoblog_s_scripts() {
 	wp_enqueue_style( 'photoblog_s-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'photoblog_s-navigation', get_template_directory_uri() . '/js/comments.js', array(), '20151215', true );
+	wp_enqueue_script( 'photoblog_s-comments', get_template_directory_uri() . '/js/comments.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'photoblog_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
